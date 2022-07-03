5eadced083610b4e88a9f77f5157e6e265a9ef5b
Godpower130 <godpower.egba@gmail.com>
2022-07-03 16:47:25 -0700

    c hello world is completed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-#include <string.h>
-
 /**
- * main - Entry point
+ *main - Entry point
  * 
- * Return: Always 0 (Success)
+ *Return: 0 after printing of the function
  */
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\n");
 
 return (0);
 }
